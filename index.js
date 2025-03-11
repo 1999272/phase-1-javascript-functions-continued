9b93652a57ebd8b22b1a326048016d6a65fa0a1e
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-03-11 14:21:50 +0300

    Adds additional Data

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// Function that returns a default activity for Saturday but allows overriding
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// Function that returns a default activity for Monday but allows overriding
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+// Function that returns another function which wraps an adjective
+function wrapAdjective(wrapper = "*") {
+    return function (adjective) {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
